320bee8c73ee9f7adf0c074129dcfa4f69bca844
Daniel Teel <daniel.teel.2@us.af.mil>
2021-01-12 15:47:56 -0700

    removed comment

--- rest/app.js
+++ rest/app.js
@@ -16,17 +16,7 @@ app.get('/details/:id', (req,res) => {
                     '404 not found'
             }))
 })
-// app.get('/api/books/:id', (req, res) => {
-//     knex('books')
-//         .select('*')
-//         .where('id', req.params.id)
-//         .then(data => res.status(200).send(data))
-//         .catch(err =>
-//             res.status(404).json({
-//                 message:
-//                     'What you are looking for is not here Homie'
-//             }))
-// })
+
 
 const appListening = app.listen(port, ()=> console.log("Listening on "+ port));
 
