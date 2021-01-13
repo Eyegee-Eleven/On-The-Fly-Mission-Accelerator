8b2ae12bdcc239d2394e6fccbeeef8699b4ba966
Daniel Teel <daniel.teel.2@us.af.mil>
2021-01-13 08:49:56 -0700

    End points /list and /common implemented

--- rest/app.js
+++ rest/app.js
@@ -6,17 +6,51 @@ const knex = require('knex')(require('./knexfile.js').development)
 app.use(express.json());
 
 app.get('/details/:id', (req,res) => {
-    knex('formf')
-        .select('*')
-        .where('id', req.params.id)
-        .then(data => res.status(200).send(data))
-        .catch(err =>
-            res.status(404).json({
-                message:
-                    '404 not found'
-            }))
+	knex('formf')
+		.select('*')
+		.where('id', req.params.id)
+		.then(data => res.status(200).send(data))
+		.catch(err =>
+			res.status(404).json({
+				message:
+					'404 not found'
+			}))
+})
+app.get('/common', (req,res) => {
+	knex('kit')
+		.select('*')
+		.then( kitData => {
+			knex('cargo')
+			.select('*')
+			.then( cargoData => {
+				res.status(200).send({kit: kitData, cargo: cargoData});
+			});
+			
+		})
+		.catch(err =>
+			res.status(404).json({
+				message:
+					'404 not found'
+			}))
 })
 
+app.get('/list', (req,res) => {
+	knex('formf')
+		.select('*')
+		.then(formfs => {
+			let listArray = [];
+			for (let formf of formfs){
+				listArray.push({id: formf.id, tail: formf.tail, mission: formf.mission, date: formf.date});
+			}
+			
+			res.status(200).send(listArray);
+		})
+		.catch(err =>
+			res.status(404).json({
+				message:
+					'404 not found'
+			}))
+})
 
 const appListening = app.listen(port, ()=> console.log("Listening on "+ port));
 
