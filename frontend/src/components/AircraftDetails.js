7a525056c7a2654507e636f5b25744a1676adc6f
Cecil Montemayor <cecilmontemayoriv@gmail.com>
2021-01-14 18:01:14 -0500

    added styling to the Aircraft Details page

--- frontend/src/components/AircraftDetails.js
+++ frontend/src/components/AircraftDetails.js
@@ -1,3 +1,5 @@
+import '../css/AircraftDetails.css'
+
 export default function AircraftDetails( {formf={}, setFormF} ){
 
 	const inputOnChange = (event) => {
@@ -6,13 +8,15 @@ export default function AircraftDetails( {formf={}, setFormF} ){
 		setFormF(newFormF);
 	}
 
-	return	<div>
-	Basic Weight: <input type='float' id='basic_weight' onChange={inputOnChange} value={formf.basic_weight}/>
-    Basic Moment: <input type='float' id='basic_moment' onChange={inputOnChange} value={formf.basic_moment}/>
-    Crew Weight: <input type='float' id='crew_weight' onChange={inputOnChange} value={formf.crew_weight}/>
-    Crew Moment: <input type='float' id='crew_moment' onChange={inputOnChange} value={formf.crew_moment}/>
-    Fuel Weight: <input type='float' id='fuel_weight' onChange={inputOnChange} value={formf.fuel_weight}/>
-    Fuel Moment: <input type='float' id='fuel_moment' onChange={inputOnChange} value={formf.fuel_moment}/>
-			</div>
+	return	(
+        <div className='inputs'>
+            Basic Weight: <input type='float' id='basic_weight' className='input' onChange={inputOnChange} value={formf.basic_weight}/>
+            Basic Moment: <input type='float' id='basic_moment' className='input' onChange={inputOnChange} value={formf.basic_moment}/>
+            Crew Weight: <input type='float' id='crew_weight' className='input' onChange={inputOnChange} value={formf.crew_weight}/>
+            Crew Moment: <input type='float' id='crew_moment' className='input' onChange={inputOnChange} value={formf.crew_moment}/>
+            Fuel Weight: <input type='float' id='fuel_weight' className='input' onChange={inputOnChange} value={formf.fuel_weight}/>
+            Fuel Moment: <input type='float' id='fuel_moment' className='input' onChange={inputOnChange} value={formf.fuel_moment}/>
+        </div>
+	)
 }
                         
