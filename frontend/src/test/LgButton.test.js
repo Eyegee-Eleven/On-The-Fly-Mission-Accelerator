b67632a69f48047d3fd1cf8ee582cc4a05e6b16c
Cecil Montemayor <cecilmontemayoriv@gmail.com>
2021-01-14 22:33:47 -0500

    fixed lg button test

--- frontend/src/test/LgButton.test.js
+++ frontend/src/test/LgButton.test.js
@@ -1,9 +1,25 @@
 import LgButton from '../components/LgButton';
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
+import StateProvider from '../state'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+  } from 'react-router-dom';
+  
 
 describe('LgButton', () => {
+    let appWrapper;
+    beforeEach(() => {
+        appWrapper = mount(
+          <StateProvider>
+            <Router>
+              <LgButton />
+            </Router>
+          </StateProvider>
+        )
+      })
     it('Creates a button', () => {
-        const appWrapper = shallow(<LgButton/>)
         const buttonMaker = appWrapper.find('button')
         expect(buttonMaker).toHaveLength(2)
     })
